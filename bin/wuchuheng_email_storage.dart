503d8f096b4cc1b8df50203078e331b1eb9099af
wuchuheng <root@wuchuheng.com>
2022-11-09 19:27:52 +0800

    init

---
+++ bin/wuchuheng_email_storage.dart
@@ -0,0 +1,5 @@
+import 'package:wuchuheng_email_storage/wuchuheng_email_storage.dart' as wuchuheng_email_storage;
+
+void main(List<String> arguments) {
+  print('Hello world: ${wuchuheng_email_storage.calculate()}!');
+}
---
+++ test/wuchuheng_email_storage_test.dart
@@ -0,0 +1,8 @@
+import 'package:wuchuheng_email_storage/wuchuheng_email_storage.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
