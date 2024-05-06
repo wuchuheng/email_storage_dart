00a1b57b50eee2068bf4fcacc095dd6435e0626f
wuchuheng <root@wuchuheng.com>
2024-05-06 13:18:38 +0800

    test(network): Add TCP client connectivity tests to email storage package
    - Increased test coverage by introducing a new test group for TCP client functionality.
    - Implemented a mock TCP server on a temporary port (4041) to simulate server connectivity.
    - Added an asynchronous test to verify successful TCP connection establishment.
    - Set a timeout of 100 seconds for the test to accommodate potential delays during execution.
    - Organized tests within the 'TCP client tests' group for clearer structure.
    
    This commit enhances the reliability of the networking component in the email storage package by ensuring its ability to establish TCP connections is thoroughly tested.

--- test/wuchuheng_email_storage_test.dart
+++ test/wuchuheng_email_storage_test.dart
@@ -1,5 +1,8 @@
-import 'package:wuchuheng_email_storage/wuchuheng_email_storage.dart';
+@Timeout(Duration(seconds: 100))
+import 'dart:io';
+
 import 'package:test/test.dart';
+import 'package:wuchuheng_email_storage/wuchuheng_email_storage.dart';
 
 void main() {
   group('A group of tests', () {
@@ -11,6 +14,28 @@ void main() {
 
     test('First Test', () {
       expect(awesome.isAwesome, isTrue);
+    });
+  });
+
+  // The group of the tests for TCP client.
+  group('TCP client tests', () {
+    // Test the TCP connection to the server.
+    test('Test TCP connection', () async {
+      // Create a TCP server that listens on port 4041
+      const tcpServerPort = 4041;
+      final tcpServerHandle =
+          await ServerSocket.bind(InternetAddress.loopbackIPv4, tcpServerPort);
+      bool isServerConnected = false;
+      // Listen for incoming connections
+      tcpServerHandle.listen((Socket client) {
+        isServerConnected = true;
+        client.close();
+        tcpServerHandle.close();
+      });
+      // Create a TCP client that connects to the server
+      await Socket.connect('127.0.0.1', tcpServerPort);
+      // Close the client
+      expect(isServerConnected, isTrue);
     });
   });
 }
